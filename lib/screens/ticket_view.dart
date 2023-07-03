731efc406f158e455e436dfbc1b0dd464fc18110
Winnard Arthur <winnardarthur7@gmail.com>
2023-07-03 19:38:53 +0000

    refactor: make height, width and some spaces dynamic

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -14,20 +14,20 @@ class TicketView extends StatelessWidget {
 
     return SizedBox(
       width: size.width * 0.85,
-      height: 200,
+      height: AppLayout.getHeight(200),
       child: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
         child: Column(
           children: [
             // Blue part of card/ticket
             Container(
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                   color: Color(0xFF526799),
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(21),
-                    topRight: Radius.circular(21),
+                    topLeft: Radius.circular(AppLayout.getHeight(21)),
+                    topRight: Radius.circular(AppLayout.getHeight(21)),
                   )),
-              padding: const EdgeInsets.all(16),
+              padding: EdgeInsets.all(AppLayout.getHeight(16)),
               child: Column(
                 children: [
                   Row(
@@ -43,7 +43,7 @@ class TicketView extends StatelessWidget {
                         child: Stack(
                           children: [
                             SizedBox(
-                              height: 24,
+                              height: AppLayout.getHeight(24),
                               child: LayoutBuilder(
                                 builder: (BuildContext context,
                                     BoxConstraints constraints) {
@@ -90,7 +90,7 @@ class TicketView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(ticket['from']['name'],
                             style: Styles.headlineStyle4
                                 .copyWith(color: Colors.white)),
@@ -99,7 +99,7 @@ class TicketView extends StatelessWidget {
                           style: Styles.headlineStyle4
                               .copyWith(color: Colors.white)),
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(ticket['to']['name'],
                             textAlign: TextAlign.end,
                             style: Styles.headlineStyle4
@@ -117,8 +117,8 @@ class TicketView extends StatelessWidget {
               child: Row(
                 children: [
                   SizedBox(
-                      height: 20,
-                      width: 10,
+                      height: AppLayout.getHeight(20),
+                      width: AppLayout.getWidth(10),
                       child: DecoratedBox(
                         decoration: BoxDecoration(
                             color: Styles.bgColor,
